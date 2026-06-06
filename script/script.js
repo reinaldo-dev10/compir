5358b911a62c6c3928c40f6079b661882afa7f5a
reinaldo-dev10 <reinaldocurso2010@hotmail.com>
2026-06-06 01:53:09 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,37 @@
+function mostrarMensagem(){
+    alert('Bem-vindo ao projeto Raízes em Movimento!');
+}
+
+const form = document.getElementById('formContato');
+const mensagem = document.getElementById('mensagem');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    mensagem.innerHTML = 'Mensagem enviada com sucesso! Obrigado pelo contato.';
+    mensagem.style.marginTop = '20px';
+    mensagem.style.color = 'green';
+
+    form.reset();
+});
+
+const btnTopo = document.getElementById('btnTopo');
+
+window.addEventListener('scroll', () => {
+
+    if(window.scrollY > 300){
+        btnTopo.classList.add('show');
+    }else{
+        btnTopo.classList.remove('show');
+    }
+
+});
+
+btnTopo.addEventListener('click', () => {
+
+    window.scrollTo({
+        top:0,
+        behavior:'smooth'
+    });
+
+});
