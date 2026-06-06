da30d31eba0565c4d61132793483e777b42c9115
reinaldo-dev10 <reinaldocurso2010@hotmail.com>
2026-06-06 02:50:28 -0300

    update agenda.html

---
+++ script/agenda.js
@@ -0,0 +1,26 @@
+const btnTopo = document.getElementById('btnTopo');
+
+window.addEventListener('scroll', () => {
+
+    if(window.scrollY > 300){
+
+        btnTopo.classList.add('show');
+
+    }else{
+
+        btnTopo.classList.remove('show');
+
+    }
+
+});
+
+btnTopo.addEventListener('click', () => {
+
+    window.scrollTo({
+
+        top:0,
+        behavior:'smooth'
+
+    });
+
+});
