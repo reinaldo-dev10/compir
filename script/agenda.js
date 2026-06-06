04f6ed7831d4742ec594c38ac1b6dd4cb4eef980
reinaldo-dev10 <reinaldocurso2010@hotmail.com>
2026-06-06 13:50:01 -0300

    update scripts

--- script/agenda.js
+++ script/agenda.js
@@ -3,24 +3,16 @@ const btnTopo = document.getElementById('btnTopo');
 window.addEventListener('scroll', () => {
 
     if(window.scrollY > 300){
-
         btnTopo.classList.add('show');
-
     }else{
-
         btnTopo.classList.remove('show');
-
     }
 
 });
 
 btnTopo.addEventListener('click', () => {
-
     window.scrollTo({
-
         top:0,
         behavior:'smooth'
-
     });
-
 });
--- script/script.js
+++ script/script.js
@@ -18,20 +18,16 @@ form.addEventListener('submit', function(event){
 const btnTopo = document.getElementById('btnTopo');
 
 window.addEventListener('scroll', () => {
-
     if(window.scrollY > 300){
         btnTopo.classList.add('show');
     }else{
         btnTopo.classList.remove('show');
     }
-
 });
 
 btnTopo.addEventListener('click', () => {
-
     window.scrollTo({
         top:0,
         behavior:'smooth'
     });
-
 });
